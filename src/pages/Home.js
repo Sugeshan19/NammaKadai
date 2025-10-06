8cb11f1a0ccdbcdf9f32ff0025352f42cbfed8d8
Sugeshan <sugeshans62@gmail.com>
2025-10-06 10:21:51 +0530

    Update Home page and CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'; 
-import Footer from '../components/footer'; 
+import Footer from '../components/footerooter'; 
+import homeImage from '../assets/home.png';
 
 export default function Home() {
   return (
     <>
-     
-      <div className="home-container">
+      <div
+        className="home-container"
+        style={{
+          backgroundImage: `url(${homeImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#fff',
+          textAlign: 'center',
+          textShadow: '0 0 5px black, 0 0 10px pink, 0 0 20px #fff, 0 0 40px blue',
+        }}
+      >
         <h1>Welcome to Namma Kadai 🛍️</h1>
         <p>Find the best deals on your favorite products!</p>
         <Link to="/shop">
           <button className="home-button">Start Shopping</button>
         </Link>
       </div>
-      <div>
-          <Footer /> 
-        </div>
 
+      <Footer />
     </>
   );
 }
